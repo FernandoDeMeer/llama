ce55a4d3b5025e5c7e747742b615ab4e39079059
FernandoDeMeer <ferdemeer@gmail.com>
2023-09-13 11:16:02 +0000

    EM matching script

--- entity_matching_attempt.py
+++ entity_matching_attempt.py
@@ -1,15 +1,12 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
 import fire
-
+import time
 from llama import Llama
 
 
 def main(
     ckpt_dir: str,
     tokenizer_path: str,
-    temperature: float = 0.2,
+    temperature: float = 0.6,
     top_p: float = 0.9,
     max_seq_len: int = 128,
     max_gen_len: int = 64,
@@ -23,28 +20,24 @@ def main(
     )
 
     prompts = [
-        # For these prompts, the expected answer is the natural continuation of the prompt
-        "I believe the meaning of life is",
-        "Simply put, the theory of relativity states that ",
-        """A brief message congratulating the team on the launch:
-
-        Hi everyone,
+        # Few shot prompt (providing a few examples before asking model to match);
+        """Do the following records refer to the same real-world entity? Answer with "Yes" if affirmative and "No" otherwise.
         
-        I just want""",
-        # Few shot prompt (providing a few examples before asking model to complete more);
-        """Translate English to French:
-        
-        sea otter => loutre de mer
-        peppermint => menthe poivrée
-        plush girafe => girafe peluche
-        cheese =>""",
+        Record 1: name: Computas Subsidiary Corp, city: Lysaker, region: Akershus, country_code: NOR, short_description: Computas Subsidiary Corp is a Norwegian IT consulting company that provides services and solutions for business processes and collaboration 
+        Record 2: name: Computas SC, country_code: Norway """,
     ]
+    t = time.time()
+
     results = generator.text_completion(
         prompts,
         max_gen_len=max_gen_len,
         temperature=temperature,
         top_p=top_p,
     )
+
+    elapsed = time.time() - t
+    print(f"Elapsed time for 1 round of inference: {elapsed:.2f}s")
+
     for prompt, result in zip(prompts, results):
         print(prompt)
         print(f"> {result['generation']}")
